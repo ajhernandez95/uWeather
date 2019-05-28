4358d4667e0b5406e17fd28a01ba364106393611
ajhernandez95 <31139761+ajhernandez95@users.noreply.github.com>
2019-05-28 16:39:29 -0500

    update

--- ui.js
+++ ui.js
@@ -13,5 +13,15 @@ class UI {
         'src',
         `http://openweathermap.org/img/w/${weather.weather[0].icon}.png`
       );
+    document.getElementById('w-humidity').textContent = `Relative Humidity: ${
+      weather.main.humidity
+    }`;
+    document.getElementById('w-press').textContent = `Atmospheric Pressure: ${
+      weather.main.pressure
+    } hPa`;
+    document.getElementById('w-min-temp').textContent =
+      'Low: ' + (((weather.main.temp_min - 273.15) * 9) / 5 + 32).toFixed(2);
+    document.getElementById('w-max-temp').textContent =
+      'High: ' + (((weather.main.temp_max - 273.15) * 9) / 5 + 32).toFixed(2);
   }
 }
